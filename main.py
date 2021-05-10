ae4d5b1efffb69fcbf6ee5fcfdd6ed180d10ebbc
Travis J Morrison <tjmorrison635@gmail.com>
2021-05-10 10:59:33 -0400

    Initial File Written
    This commit we begin to load data from WBA and plot it

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+df = pd.read_csv("C:/Users/tjmor/OneDrive/Documents/Consulting/UAC_beacon/WBA_data/BCC Bear Trap RAW.csv")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print(df.head(5))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+df.plot(0, [1], subplots=False)
